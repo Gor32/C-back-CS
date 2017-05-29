f81b776ec4e294b92de87e0a82e6aaf51c01e44a
Gor32 <kirakosyan0001@gmail.com>
2017-05-29 19:21:48 +0400

    Add files via upload

---
+++ Facebook/FacebookLike.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace Facebook
+{
+    public class FacebookLike
+    {
+        private static string GetWebText(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.UserAgent = "A .NET Web Crawler";
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string htmlText = reader.ReadToEnd();
+            return htmlText;
+        }
+        public static void PrintLikeCount(string pageUrl)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var jsonCode = GetWebText(pageUrl);
+            dynamic dy_item = serializer.Deserialize<object>(jsonCode);
+            for (int i = 0; i < dy_item["posts"]["data"].Length; i++) {
+                Console.WriteLine("Like = "+dy_item["posts"]["data"][i]["likes"]["summary"]["total_count"]);
+            }
+            
+        }
+
+    }
+}
---
+++ Facebook/Program.cs
@@ -0,0 +1,12 @@
+﻿using Facebook;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string url = "https://graph.facebook.com/"+input+"/?fields=posts.limit(5){likes.summary(true)}&access_token=EAACEdEose0cBABO2cSTMvB0qPAmaIFXo2HHs1SYc0ncbhPqouwV42y1dYCpY43VOzhk4062qx28I5ENUnOHnkgfuM4ILysrFQZAOo8ZCl659Nh8U7F8S8mxq0JZCyfvtWHbk83jme8BAxHICpxtgch2SXzTOqVFKvyyvX80fwxBZAcr14VTN4XXDHCIVc2cZD";
+        FacebookLike.PrintLikeCount(url);
+    }
+}
