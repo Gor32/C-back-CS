15ea75a9a1f01c2d2cc95b4f3a13652b44a6bc81
Gor32 <kirakosyan0001@gmail.com>
2017-06-14 16:01:05 +0400

    Create ServerObject.cs

--- Client Server/Server/ServerObject.cs
+++ Client Server/Server/ServerObject.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
@@ -6,33 +6,33 @@ using System.Net;
 using System.Text;
 using System.Threading;
 
-namespace ChatServer
+namespace PrivateChat
 {
     public class ServerObject
     {
-        /// <summary>
-        /// Server for Listen
-        /// </summary>
+
         static TcpListener tcpListener;
-        /// <summary>
-        /// All connectivity users
-        /// </summary>
-        List<ClientObject> clients = new List<ClientObject>(); 
 
+        List<ClientObject> clients = new List<ClientObject>();
 
-        /// <summary>
-        /// Add ClientObject to clients List 
-        /// </summary>
-        /// <param name="clientObject"></param>
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
         protected internal void AddConnection(ClientObject clientObject)
         {
             clients.Add(clientObject);
         }
 
-        /// <summary>
-        /// RemoveConnection by ID
-        /// </summary>
-        /// <param name="id">ID user</param>
         protected internal void RemoveConnection(string id)
         {
             ClientObject client = clients.FirstOrDefault(c => c.Id == id);
@@ -40,15 +40,36 @@ namespace ChatServer
                 clients.Remove(client);
         }
 
-        /// <summary>
-        /// Listening for incoming connections
-        /// </summary>
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); //Stop server
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); //Disconnect clinets
+            }
+            Environment.Exit(0); //end of process
+        }
+
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id)
+                {
+                    clients[i].Stream.Write(data, 0, data.Length);
+                }
+            }
+        }
         protected internal void Listen()
         {
             try
             {
-                tcpListener = new TcpListener(IPAddress.Any,8888);
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
                 tcpListener.Start();
+
+                Console.WriteLine("Connect on this "+ GetLocalIPAddress()+" IP");
                 Console.WriteLine("Server started. Wait clinets...");
 
                 while (true)
@@ -66,36 +87,6 @@ namespace ChatServer
                 Disconnect();
             }
         }
-
-        /// <summary>
-        ///  Broadcast message to connected clients
-        /// </summary>
-        /// <param name="message">Message</param>
-        /// <param name="id">User id</param>
-        protected internal void BroadcastMessage(string message, string id)
-        {
-            byte[] data = Encoding.Unicode.GetBytes(message);
-            for (int i = 0; i < clients.Count; i++)
-            {
-                if (clients[i].Id != id) 
-                {
-                    clients[i].Stream.Write(data, 0, data.Length);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Disabling all clients
-        /// </summary>
-        protected internal void Disconnect()
-        {
-            tcpListener.Stop(); //Stop server
-
-            for (int i = 0; i < clients.Count; i++)
-            {
-                clients[i].Close(); //Disconnect clinets
-            }
-            Environment.Exit(0); //end of process
-        }
     }
+
 }
